f98e606c26a422808718f6b3b425312b8749b58e
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-10-28 12:14:50 +0100

    feat(vector): initial class header gh-21

---
+++ src/include/raymath/Vector.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+
+class Vector {
+ private:
+  int x = 0;
+  int y = 0;
+  int z = 0;
+
+ public:
+  Vector();
+  Vector(int x, int y, int z);
+  ~Vector();
+
+  int getX();
+  int getY();
+  int getZ();
+
+  void setX(int x);
+  void setY(int y);
+  void setZ(int z);
+
+  Vector operator+(Vector const& vect);
+  Vector operator*(Vector const& vect);
+  Vector operator*(float const& vect);
+  Vector operator-(Vector const& vect);
+  Vector operator=(Vector const& vect);
+  friend std::ostream& operator<<(std::ostream& _stream, Vector const& vect);
+
+  float getNorm();
+  Vector normalize();
+};
