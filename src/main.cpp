8e98c36c032a8489095290b56465a821965d65dd
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-10-28 14:12:49 +0100

    feat(color): intensity adjustement

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@ using namespace std;
 
 int main() {
   Color red(1, 0, 0);
+  Color redLowIntensity = red * 0.24;
   Color green(0, 1, 0);
   Color black;
 
   cout << "Red : " << red << std::endl;
+  cout << "Red low intensity : " << redLowIntensity << std::endl;
   cout << "Green : " << green << std::endl;
   cout << "Black : " << black << std::endl;
 
@@ -23,10 +25,12 @@ int main() {
   // Create an image in memory, and fill it with yellow
   Image image(512, 512, yellow);
 
+  Color testColor = red * 0.5;
+
   // Make a red square on the top left of the image
   for (int y = 0; y < 100; y++) {
     for (int x = 0; x < 100; x++) {
-      image.SetPixel(x, y, Color(1, 0, 0));
+      image.SetPixel(x, y, redLowIntensity);
     }
   }
 
